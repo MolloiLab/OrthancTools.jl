1d1487e5bf2efea9d4a1b9430721a6cdf0feac01
Dale-Black <djblack@uci.edu>
2023-01-19 15:40:24 -0800

    refactor into one major loop

--- src/downloader.jl
+++ src/downloader.jl
@@ -20,10 +20,10 @@ ip_address = "128.200.49.26"
 
 # ╔═╡ 93bc7b5c-97dc-4166-89e8-490e5366c7cb
 md"""
-## Get Series
+## Get Studies
 """
 
-# ╔═╡ bb6f2c23-a540-4474-a9e5-89ba76a57e64
+# ╔═╡ 8e780f7b-53df-4e0b-bf3d-b4c1094b7b24
 """
 ```julia 
 get_all_studies(ip_address::String="localhost")
@@ -36,41 +36,31 @@ function get_all_studies(ip_address::String="localhost")
 	url_studies = joinpath(url, "studies")
 	studies = JSON.parse(String(HTTP.get(url_studies).body))
 
-	studies_clean = []
+	studies_dict = OrderedDict{String, Vector}()
 	for study in studies
 		s = JSON.parse(String(HTTP.get(joinpath(url_studies, study)).body))
 		try
 			accession_num = s["MainDicomTags"]["AccessionNumber"]
-			push!(studies_clean, study)
+			if !haskey(studies_dict, accession_num)
+				push!(studies_dict, accession_num => [study])
+			else
+				push!(studies_dict[accession_num], study)
+			end
 		catch
 			@warn "No accession number for $study"
 		end
 	end
-
-	studies_dict = OrderedDict{String, Vector}()
-	for study in studies_clean
-		s = JSON.parse(String(HTTP.get(joinpath(url_studies, study)).body))
-		accession_num = s["MainDicomTags"]["AccessionNumber"]
-		if !haskey(studies_dict, accession_num)
-			push!(studies_dict, accession_num => [study])
-		else
-			push!(studies_dict[accession_num], study)
-		end
-	end
+	
 	return studies_dict
 end
 
-# ╔═╡ c82433a8-db7e-4951-8476-8c8fa2ac023b
+# ╔═╡ 11082ead-55a8-48e7-9bbe-7856fa8d7903
 study_dict = get_all_studies(ip_address)
-
-# ╔═╡ 8e780f7b-53df-4e0b-bf3d-b4c1094b7b24
-
 
 # ╔═╡ Cell order:
 # ╠═d8578dac-9845-11ed-324c-856950ed40c3
 # ╠═0771ec75-e37b-4fa8-906d-a6b1f3f51d6f
 # ╠═a8d7f303-4b86-4172-9425-da1c5c0daa66
 # ╟─93bc7b5c-97dc-4166-89e8-490e5366c7cb
-# ╠═bb6f2c23-a540-4474-a9e5-89ba76a57e64
-# ╠═c82433a8-db7e-4951-8476-8c8fa2ac023b
 # ╠═8e780f7b-53df-4e0b-bf3d-b4c1094b7b24
+# ╠═11082ead-55a8-48e7-9bbe-7856fa8d7903
