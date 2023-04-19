33cf7b7904b4f6748e6adcb24290144959713b73
Dale-Black <djblack@uci.edu>
2023-04-19 09:30:07 -0700

    update GUI to wrap all inputs with one submit

--- docs/quickstart.jl
+++ docs/quickstart.jl
@@ -22,6 +22,7 @@ begin
 	Pkg.add(url="https://github.com/Dale-Black/OrthancTools.jl")
 	Pkg.add(["PlutoUI", "DICOM"])
     using PlutoUI, DICOM, OrthancTools
+	using PlutoUI: combine
 end
 
 # ╔═╡ 8868c105-5209-4479-80cb-bb0423b66524
@@ -53,23 +54,57 @@ studies_dict = get_all_studies(ip_address)
 # ╔═╡ b1e145de-16fa-4adc-886b-e1260b9a6fa2
 md"""
 ## Get Series
-Insert the accession number into the input box below and click "Submit". When the code is finished, you can inspect the files by clicking on the dictionary.
+Insert the accession number into the input box above and click "Submit". When the code is finished, you can inspect the files by clicking on the dictionary.
 """
-
-# ╔═╡ cc9fb05a-cfd6-45da-89ab-2315ae0cf08d
-@bind accession_number confirm(TextField(default="2581"))
-
-# ╔═╡ a07a479a-830a-4a8f-a4de-1dc0b8156d8f
-series_dict = get_all_series(studies_dict, accession_number, ip_address)
 
 # ╔═╡ 1b64364c-7b47-41d9-aa09-91c37fc95183
 md"""
 ## Get Instance(s)
-You can insert the series number of interest into the input box below and then click "Submit". When the code is finished, you can inspect the files by clicking on the dictionary.
+You can insert the series number of interest into the input box above and then click "Submit". When the code is finished, you can inspect the files by clicking on the dictionary.
 """
 
-# ╔═╡ b379470c-d3c8-48b1-875c-2caee044897b
-@bind series_num confirm(TextField(default="1, 2"))
+# ╔═╡ 388bb92b-fc3f-4054-977a-1ca655c16e40
+md"""
+# Download DICOM Instance(s)
+Type the folder path above, where you want the DICOM files to be saved (or use a temporary directory via `mktempdir()`) in the code cell below. Then type in the instance number that you want to download and click "Submit".
+"""
+
+# ╔═╡ 01d891cf-b48f-44a2-aea7-5af27c6ea1b4
+function download_info(acc, ser, inst, save_folder_path)
+	
+	return PlutoUI.combine() do Child
+		
+		inputs = [
+			md""" $(acc): $(
+				Child(TextField(default="2581"))
+			)""",
+			md""" $(ser): $(
+				Child(TextField(default="1, 2"))
+			)""",
+			md""" $(inst): $(
+				Child(TextField(default="1"))
+			)""",
+			md""" $(save_folder_path): $(
+				Child(TextField(default=(mktempdir())))
+			)"""
+		]
+		
+		md"""
+		#### Scan Details
+		Input the relevant DICOM information to download the appropriate scans
+		$(inputs)
+		"""
+	end
+end
+
+# ╔═╡ 46d908ed-f841-4938-899f-e65de3004574
+@bind details confirm(download_info("Accession Number", "Series Number(s)", "Instance Number", "Output Directory"))
+
+# ╔═╡ 265f6407-62b2-483c-a803-05b94afe7e3f
+accession_number, series_num, instance_num, output_dir = details
+
+# ╔═╡ a07a479a-830a-4a8f-a4de-1dc0b8156d8f
+series_dict = get_all_series(studies_dict, accession_number, ip_address)
 
 # ╔═╡ 31f97455-006b-4ec7-a6df-0f3980b77835
 series_num_vec = parse.(Int, split(series_num, ","))
@@ -83,17 +118,8 @@ begin
 	end
 end
 
-# ╔═╡ 388bb92b-fc3f-4054-977a-1ca655c16e40
-md"""
-# Download DICOM Instance(s)
-Type the folder path below, where you want the DICOM files to be saved (or use a temporary directory via `mktempdir()`) in the code cell below. Then type in the instance number that you want to download and click "Submit".
-"""
-
-# ╔═╡ a8d994d6-cbb0-416c-852b-d0d173195444
-output_dir = mktempdir() # insert folder path here "Z:\\example\\path\\to\\folder"
-
-# ╔═╡ 6ec25c7e-4982-4f55-ae50-48880be2062c
-@bind instance_num confirm(TextField(default="1"))
+# ╔═╡ b8182048-77e2-47f1-8171-cc7181990ff0
+instances_dicts
 
 # ╔═╡ b8747a52-61a2-46ec-b5c6-8f29406251da
 instance_number = parse(Int64, instance_num)
@@ -116,18 +142,18 @@ dcms = dcmdir_parse(output_path)
 # ╠═38c7cb4b-b317-4de4-82db-39fcb8117215
 # ╠═48008e07-b10d-4e37-853f-0169fe2cb4b1
 # ╟─85c694aa-15d9-4da4-8dbe-0a0e02563761
-# ╠═ee188b2c-d928-48be-a52e-d3fbdb3a3f70
+# ╟─ee188b2c-d928-48be-a52e-d3fbdb3a3f70
 # ╠═399ba33a-a44d-4395-a523-f17fab9149d9
+# ╟─46d908ed-f841-4938-899f-e65de3004574
+# ╠═265f6407-62b2-483c-a803-05b94afe7e3f
 # ╟─b1e145de-16fa-4adc-886b-e1260b9a6fa2
-# ╠═cc9fb05a-cfd6-45da-89ab-2315ae0cf08d
 # ╠═a07a479a-830a-4a8f-a4de-1dc0b8156d8f
 # ╟─1b64364c-7b47-41d9-aa09-91c37fc95183
-# ╠═b379470c-d3c8-48b1-875c-2caee044897b
 # ╠═31f97455-006b-4ec7-a6df-0f3980b77835
 # ╠═8f777827-6ddf-4421-809d-d0d8858542b4
+# ╠═b8182048-77e2-47f1-8171-cc7181990ff0
 # ╟─388bb92b-fc3f-4054-977a-1ca655c16e40
-# ╠═a8d994d6-cbb0-416c-852b-d0d173195444
-# ╠═6ec25c7e-4982-4f55-ae50-48880be2062c
 # ╠═b8747a52-61a2-46ec-b5c6-8f29406251da
 # ╠═8268df02-e085-4245-a3bc-5fa354bbad4e
 # ╠═ece4cb9b-4eee-4317-b835-c803ee2781ef
+# ╟─01d891cf-b48f-44a2-aea7-5af27c6ea1b4
